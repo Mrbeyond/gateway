456ecc522bd9ac1a840a921d7d4eedb6aacb64eb
Taiwo <enochtaiwotimothy@gmail.com>
2021-02-14 23:20:37 +0100

    dashboard in progress

--- src/views/app/Dashboard.vue
+++ src/views/app/Dashboard.vue
@@ -6,9 +6,37 @@
             <div class="separator mb-5"></div>
         </b-colxx>
     </b-row>
-    <b-row>
-        <b-colxx sm="12" md="6" lg="6">
-            <b-card class="mb-4" title="Your profile">
+    <b-row class="justify-content-center mb-5">
+        <b-colxx sm="12" md="6" lg="7" class="mr-lg-5">
+            <b-card class="mb-4" title="Profile">
+          <div class="row mb-3">
+            <strong class="col-lg-6 col-md-6 col-sm-6 col-12 ">First name</strong>  <span class=" col-sm-6 col-lg-6 col-md-6 col-12  text-lg-right text-md-right"> {{businesses.firstname}}</span>
+                </div>
+                <hr>
+             <div class="row mb-3">
+            <strong class="col-lg-6 col-md-6 col-12 ">Last name</strong>  <span class="col-lg-6 col-md-6 col-12  text-lg-right text-md-right"> {{businesses.lastname}}</span>
+             </div>
+                <hr>
+
+               <div class="row mb-3">
+            <strong class="col-lg-6 col-md-6 col-12 ">Phone</strong>  <span class="col-lg-6 col-md-6 col-12  text-lg-right text-md-right"> {{businesses.phone}}</span>
+             </div>
+                <hr>
+
+                <div class="row mb-3">
+            <strong class="col-lg-6 col-md-6 col-12 ">Email</strong>  <span class="col-lg-6 col-md-6 col-12  text-lg-right text-md-right"> {{businesses.email}}</span>
+             </div>
+                 <!-- <div class="row mb-3">
+            <strong class="col-lg-6 col-md-6 col-12 ">Status</strong>  <span class="col-lg-6 col-md-6 col-12  text-lg-right text-md-right"> {{ 
+                callback(businesses.status) 
+               
+                }}</span>
+             </div> -->
+
+            </b-card>
+        </b-colxx>
+        <b-colxx sm="12" md="6" lg="4">
+            <b-card class="mb-4" title="Account">
                 <div>
             <strong>First name</strong>  <span class="float-right"> {{businesses.firstname}}</span>
                 </div>
@@ -18,35 +46,15 @@
                <div>
             <strong>Phone</strong>  <span class="float-right"> {{businesses.phone}}</span>
              </div>
-
+        
             </b-card>
         </b-colxx>
-         <b-colxx sm="12" md="6" lg="6">
-            <b-card class="mb-4" title="Your profile">
-                <div>
-            <strong>First name</strong>  <span class="float-right"> {{businesses.firstname}}</span>
-                </div>
-             <div>
-            <strong>Last name</strong>  <span class="float-right"> {{businesses.lastname}}</span>
-             </div>
-               <div>
-            <strong>Phone</strong>  <span class="float-right"> {{businesses.phone}}</span>
-             </div>
-
-            </b-card>
-        </b-colxx>
+        
     </b-row>
-     <div>
-    <!-- <div v-if="isLoading && !isFetched" class="row justify-content-center">
-        <div> <b-spinner variant="primary" /></div>
-    </div> -->
-    <!-- <div v-else-if="!isLoading && !isFetched">
-        Went wrong slot
-    </div> -->
-    <b-row>
-      <b-colxx xxs="12" class="mt-4">
-        <h2 class="text-center mb-5">BUSINESSES</h2>
-
+    <b-row class="justify-content-center">
+          <b-colxx sm="12" md="12" lg="7" class="mr-lg-5">
+        <!-- <b-card class="row"> -->
+             <h2 class="text-center mb-5">BUSINESS</h2>
         <vuetable
           ref="vuetable"
           class="table-divided order-with-arrow"
@@ -57,29 +65,26 @@
           :fields="fields"
           pagination-path
         >
-          <!-- <template  slot="garages" slot-scope="props" >
-            <div v-if="props">
-            <router-link :to="`/dashboard/garages/${props.rowData.id}`" v-if="props.rowData.garages.length>=1">
-            <b-btn  title="View Vehicles" badge-variant="dark" v-if="props"  v-b-modal.modalbasic
-            >
-              View <b-badge variant="primary" rounded-conner>{{props.rowData.garages.length}}</b-badge>
-            </b-btn>
-            </router-link>
-            <b-btn v-else :disabled="props.rowData.garages.length==0"  title="View Vehicles" badge-variant="dark"  v-b-modal.modalbasic
-            >
-              View <b-badge variant="primary" rounded-conner>{{props.rowData.garages.length}}</b-badge>
-            </b-btn>
-            </div>
-          </template> -->
         </vuetable>
-        <!-- <vuetable-pagination-bootstrap
-          class="mt-4"
-          ref="pagination"
-          @vuetable-pagination:change-page="onChangePage"
-        /> -->
-      </b-colxx>
+        <!-- </b-card> -->
+        </b-colxx>
+         
+         <b-colxx sm="12" md="6" lg="4">
+            <b-card class="mb-4" title="Activity logs">
+                <div>
+            <strong>First name</strong>  <span class="float-right"> {{businesses.firstname}}</span>
+                </div>
+             <div>
+            <strong>Last name</strong>  <span class="float-right"> {{businesses.lastname}}</span>
+             </div>
+               <div>
+            <strong>Phone</strong>  <span class="float-right"> {{businesses.phone}}</span>
+             </div>
+        
+
+            </b-card>
+        </b-colxx>
     </b-row>
-  </div>
 </div>
 </template>
 <script>
